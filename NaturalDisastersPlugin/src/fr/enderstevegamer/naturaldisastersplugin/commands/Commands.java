c566b686c3863a75930e9d489c2adba3871b1bd2
EndersteveGamer <85014963+EndersteveGamer@users.noreply.github.com>
2022-09-25 12:05:02 +0200

    Removed things

--- NaturalDisastersPlugin/src/fr/enderstevegamer/naturaldisastersplugin/commands/Commands.java
+++ NaturalDisastersPlugin/src/fr/enderstevegamer/naturaldisastersplugin/commands/Commands.java
@@ -15,40 +15,9 @@ public class Commands implements CommandExecutor {
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
             if (command.getName().equalsIgnoreCase("triggerEvent")) {
-                if (strings.length == 0) {
-                    RandomEvent event = new RandomEvent();
-                    event.RandomEvent(true);
-                    player.sendMessage("Random event triggered!");
-                }
-                else {
-                    if (strings[0].equals("Tntrain")) {
-                        Tntrain.tntrain();
-                        player.sendMessage("Tntrain event triggered!");
-                    }
-                    else if (strings[0].equals("Darkness")) {
-                        Darkness.darkness();
-                        player.sendMessage("Darkness event triggered!");
-                    }
-                    else if (strings[0].equals("Lava")) {
-                        Lava.lava();
-                        player.sendMessage("Lava event triggered!");
-                    }
-                    else if (strings[0].equals("ZombieInvasion")) {
-                        ZombieInvasion.zombieInvasion();
-                        player.sendMessage("ZombieInvasion event triggered!");
-                    }
-                    else if (strings[0].equals("Flood")) {
-                        Flood.flood();
-                        player.sendMessage("Flood event triggered!");
-                    }
-                    else if (strings[0].equals("Floatingisland")) {
-                        Floatingisland.floatingisland();
-                        player.sendMessage("Floatingisland event triggered!");
-                    }
-                    else {
-                        player.sendMessage("Invalid event name!");
-                    }
-                }
+                RandomEvent event = new RandomEvent();
+                event.RandomEvent(true);
+                player.sendMessage("Event triggered!");
             }
         }
         return false;
